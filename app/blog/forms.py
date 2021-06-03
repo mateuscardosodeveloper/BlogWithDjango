31f616b8752fb6f38cff87e06eeee43e9f371235
raiizoor <mateus.informatica44@gmail.com>
2021-06-03 16:39:45 -0300

    Finished first steps

--- app/blog/forms.py
+++ app/blog/forms.py
@@ -12,14 +12,14 @@ class BlogPostForm(forms.Form):
 class BlogPostModelForm(forms.ModelForm):
     class Meta:
         model = BlogPost
-        fields = ['title', 'slug', 'content']
+        fields = ['title', 'image', 'slug', 'content', 'publish_date']
 
     def clean_title(self, *args, **kwargs):
         instance = self.instance
         title = self.cleaned_data.get("title")
         queryset = BlogPost.objects.filter(title__iexact=title) #iexact valid information if have uppercase or not
-        if instace is not None:
-            qs = qs.exclude(pk=instance.pk) #id=instance.pk
+        if instance is not None:
+            qs = queryset.exclude(pk=instance.pk) #id=instance.pk
         if queryset.exists():
             raise forms.ValidationError("This title has already been used. Please try again.")
         return title
--- app/blog/views.py
+++ app/blog/views.py
@@ -7,16 +7,19 @@ from .forms import BlogPostModelForm
 
 
 def blog_post_list_view(request):
-    query = BlogPost.objects.all()
+    qs = BlogPost.objects.all().published()
+    if request.user.is_authenticated:
+        my_qs = BlogPost.objects.filter(user=request.user)
+        qs = (qs | my_qs).distinct()
     template_name = 'blog/list.html'
-    context = {'object_list': query}
+    context = {'object_list': qs}
     return render(request, template_name, context)
 
 
 # @login_required
 @staff_member_required
 def blog_post_create_view(request):
-    form = BlogPostModelForm(request.POST or None)
+    form = BlogPostModelForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         obj = form.save(commit=False)
         obj.user = request.user
@@ -37,7 +40,7 @@ def blog_post_detail_view(request, slug):
 @staff_member_required
 def blog_post_update_view(request, slug):
     obj = get_object_or_404(BlogPost, slug=slug)
-    form = BlogPostModelForm(request.POST or None, instance=obj)
+    form = BlogPostModelForm(request.POST or None, request.FILES or None, instance=obj)
     if form.is_valid():
         form.save()
     template_name = 'form.html'
